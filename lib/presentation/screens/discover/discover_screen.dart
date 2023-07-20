aa2da28f4c28f7771e64eb4d65fc47d366179f57
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-20 19:42:29 -0400

    Add PageView

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tik_tok_video/presentation/providers/discover_provider.dart';
+import 'package:tik_tok_video/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Discover Screen") 
-        )
+
+    final discoverProvider = context.watch<DiscoverProvider>();
+    //final otroProvider = Provider.of<DiscoverProvider>(context);
+
+    return Scaffold(
+      body: discoverProvider.initialloading
+        ? const Center(child: CircularProgressIndicator( strokeWidth: 2))
+        : VideoScrollableView(videos: discoverProvider.videos)
     );
   }
 }
