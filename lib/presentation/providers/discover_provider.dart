f9161f2ca7899fdaf0a23655b90647fb0afaeea3
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-20 18:51:22 -0400

    Add loadVideos to provider

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:tik_tok_video/domain/entities/video_post.dart';
+import 'package:tik_tok_video/infrastructure/models/local_video_model.dart';
+import 'package:tik_tok_video/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   
+  //todo: Repository, DataSource
+
   bool initialloading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    //todo: load videos
 
+    await Future.delayed(const Duration(seconds: 2));
+    
+    final List<VideoPost> newVideos = videoPosts.map(
+      (video) => VideoPostModel.fromJson(video).toVideoPostEntity()
+    ).toList();
+
+    videos.addAll(newVideos);
+    initialloading = false;
     notifyListeners();
   }
 
