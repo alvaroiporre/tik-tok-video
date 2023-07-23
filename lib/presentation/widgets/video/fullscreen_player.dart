79e2aa621e11957cb6c3b756a8eea822bf90c376
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-23 12:41:24 -0400

    Add VideoplayerController

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
 
   final String videoUrl;
   final String caption;
@@ -12,7 +13,36 @@ class FullScreenPlayer extends StatelessWidget {
     });
 
   @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.asset(widget.videoUrl)
+    ..setVolume(0)
+    ..setLooping(true)
+    ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context, snapshot) {
+        return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+      },
+    );
   }
 }
