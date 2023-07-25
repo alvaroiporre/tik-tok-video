7e93dde294f4ed7c14447e6feddd3c5a1461eadb
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-24 20:46:04 -0400

    Add LocalVideoDatasource impl

---
+++ lib/infrastructure/datasources/local_videos_datasources_impl.dart
@@ -0,0 +1,22 @@
+import 'package:tik_tok_video/domain/datasources/video_posts_datasource.dart';
+import 'package:tik_tok_video/domain/entities/video_post.dart';
+import 'package:tik_tok_video/infrastructure/models/local_video_model.dart';
+import 'package:tik_tok_video/shared/data/local_video_posts.dart';
+
+class LocalVideoDatasource implements VideoPostDatasource {
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) async{
+    await Future.delayed(const Duration(seconds: 2));
+    final List<VideoPost> newVideos = videoPosts.map(
+      (video) => VideoPostModel.fromJson(video).toVideoPostEntity()
+    ).toList();
+
+    return newVideos;
+  }
+
+}
