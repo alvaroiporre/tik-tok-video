fe8ecc6ea9281b5ec8a27474b8bbaf401bfac207
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-24 20:34:24 -0400

    Add repository abstract class

---
+++ lib/domain/repositories/video_posts_repository.dart
@@ -0,0 +1,11 @@
+import 'package:tik_tok_video/domain/entities/video_post.dart';
+
+abstract class VideoPostRepository {
+
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID);
+
+
+  Future<List<VideoPost>> getTrendingVideosByPage(int page);
+
+
+}
