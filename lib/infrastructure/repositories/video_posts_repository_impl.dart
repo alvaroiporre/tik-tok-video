3cbcedd14b00a7e49b83c0e0228ea7a57a4082cd
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-07-24 20:52:25 -0400

    Add LocalVideoRepository impl

---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:tik_tok_video/domain/datasources/video_posts_datasource.dart';
+import 'package:tik_tok_video/domain/entities/video_post.dart';
+import 'package:tik_tok_video/domain/repositories/video_posts_repository.dart';
+
+class VideoPostsRepository implements VideoPostRepository {
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepository({
+    required this.videosDatasource
+  });
+  
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+  
+}
